21d0369d58edab68999e8d49712fc3230cb06e6c
Md. Al Amin <alamin.munna@gmail.com>
2025-10-23 13:33:17 +0600

    Implement Cart Modal

---
+++ src/store/UserProgressContext.jsx
@@ -0,0 +1,45 @@
+import { useContext, useState } from "react";
+
+const UserProgressContext = useContext({
+  progress: "",
+  showCart: () => {},
+  hideCart: () => {},
+  showCheckout: () => {},
+  hideCheckout: () => {},
+});
+
+export function UserProgressContextProvider({ children }) {
+  const [userProgress, setUserProgress] = useState("");
+
+  function showCart() {
+    setUserProgress("cart");
+  }
+
+  function hideCart() {
+    setUserProgress("");
+  }
+
+  function showCheckout() {
+    setUserProgress("checkout");
+  }
+
+  function hideCheckout() {
+    setUserProgress("");
+  }
+
+  const userProgressCtx = {
+    progress: userProgress,
+    showCart,
+    hideCart,
+    showCheckout,
+    hideCheckout,
+  };
+
+  return (
+    <UserProgressContext value={userProgressCtx}>
+      {children}
+    </UserProgressContext>
+  );
+}
+
+export default UserProgressContext;
